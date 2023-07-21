69039a36f62862be6f645fff628b7d1ca1b3d64a
Kenneth Kang <kkang@paypal.com>
2023-07-21 11:55:39 -0700

    test chksum fail, chk oci.h

--- tests/oracle.sh
+++ tests/oracle.sh
@@ -16,12 +16,13 @@ ss -tln
 pushd worker/cppworker/worker
 sudo apt install libboost-regex-dev -y
 wget https://download.oracle.com/otn_software/linux/instantclient/1919000/instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/1919000/instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip
-( echo SHA384 \(instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip\) = bb68094a12e754fc633874e8c2b4c4d38a45a65a5a536195d628d968fca72d7a5006a62a7b1fdd92a29134a06605d2b4 ; echo SHA384 \(instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip\) = 5999f2333a9b73426c7af589ab13480f015c2cbd82bb395c7347ade37cc7040a833a398e9ce947ae2781365bd3a2e371 ) | sha384sum -c -
+( echo SHA384 \(instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip\) = bb68094a12e754fc633874e8c2b4c4d38a45a65a5a536195d628d968fca72d7a5006a62a7b1fdd92a29134a06605d2b4 ; echo SHA384 \(instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip\) = 5999f2333a9b73426c7af589ab13480f015c2cbd82bb395c7347ade37cc7040a833a398e9ce947ae2781365bd3a2e370 ) | sha384sum -c -
 pubdir=$PWD
 pushd /opt
 ln -s instantclient_19_19 instantclient_19_17
 unzip $pubdir/*.zip
 popd
+ls -l /opt/instantclient_19_17//sdk/include/oci.h
 make -f ../build/makefile19
 
 #origSh# suites="bind_eviction_tests strandedchild_tests coordinator_tests saturation_tests adaptive_queue_tests rac_tests sharding_tests"
