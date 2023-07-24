504dc5132bd3555a01e8523099ee30039ce425cb
Kenneth Kang <kkang@paypal.com>
2023-07-24 10:11:13 -0700

    direct two task

--- tests/oracle.sh
+++ tests/oracle.sh
@@ -18,6 +18,7 @@ wget -nv https://download.oracle.com/otn_software/linux/instantclient/1919000/in
 curl -O https://download.oracle.com/otn_software/linux/instantclient/1919000/instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip
 echo bb68094a12e754fc633874e8c2b4c4d38a45a65a5a536195d628d968fca72d7a5006a62a7b1fdd92a29134a06605d2b4  instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip >> SHA384
 echo 5999f2333a9b73426c7af589ab13480f015c2cbd82bb395c7347ade37cc7040a833a398e9ce947ae2781365bd3a2e371  instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip >> SHA384
+## ???? sqlplus for verification
 sha384sum -c SHA384
 pubdir=$PWD
 pushd /opt
@@ -31,6 +32,7 @@ cp -v oracleworker $GOPATH/bin/
 popd
 
 # run test with oracle
+export TWO_TASK='(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XEPDB1)(FAILOVER_MODE=(TYPE=SESSION)(METHOD=BASIC)(RETRIES=1000)(DELAY=5))))'
 d=oracleHighLoadAdj
 pushd $GOPATH/src/github.com/paypal/hera/tests/unittest2/$d
 cp -v $GOPATH/bin/oracleworker .
