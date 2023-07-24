994faa6923d805e367b2ab1ef4b9a2977ad68ec4
Kenneth Kang <kkang@paypal.com>
2023-07-24 16:01:09 -0700

    sqlplus

--- tests/oracle.sh
+++ tests/oracle.sh
@@ -16,17 +16,20 @@ fi
 # make oracle worker
 pushd worker/cppworker/worker
 sudo apt install libboost-regex-dev -y
-wget -nv https://download.oracle.com/otn_software/linux/instantclient/1919000/instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip 
+wget -nv https://download.oracle.com/otn_software/linux/instantclient/1919000/instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/1920000/instantclient-sqlplus-linux.x64-19.20.0.0.0dbru.zip
 curl -O https://download.oracle.com/otn_software/linux/instantclient/1919000/instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip
+echo 409b867f76c701ccba47f9278363b204137fc92444c317b36b60da35669453a99bd02a3c84b1b9b92c54fd94929a0eff  instantclient-sqlplus-linux.x64-19.20.0.0.0dbru.zip >> SHA384
 echo bb68094a12e754fc633874e8c2b4c4d38a45a65a5a536195d628d968fca72d7a5006a62a7b1fdd92a29134a06605d2b4  instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip >> SHA384
 echo 5999f2333a9b73426c7af589ab13480f015c2cbd82bb395c7347ade37cc7040a833a398e9ce947ae2781365bd3a2e371  instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip >> SHA384
 ## ???? sqlplus for verification
 sha384sum -c SHA384
 pubdir=$PWD
 pushd /opt
+ln -s instantclient_19_19 instantclient_19_20
 ln -s instantclient_19_19 instantclient_19_17
 unzip $pubdir/instantclient-basiclite-linux.x64-19.19.0.0.0dbru.zip
 unzip $pubdir/instantclient-sdk-linux.x64-19.19.0.0.0dbru.zip
+unzip $pubdir/instantclient-sqlplus.x64-19.20.0.0.0dbru.zip
 popd
 cat /proc/loadavg
 make -f ../build/makefile19 -j 3
@@ -51,6 +54,7 @@ else
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
 fi
 echo LD_LIBRARY_PATH $LD_LIBRARY_PATH
+$ORACLE_HOME/sqlplus $username/$password@$TWO_TASK
 
 # run test with oracle
 d=oracleHighLoadAdj
