9272a82e3e7d080c5d3f35f92cf71ee748fab4a1
Kenneth Kang <kkang@paypal.com>
2023-08-18 15:29:59 -0700

    run short list, find all tests

--- tests/functionaltest/runall.sh
+++ tests/functionaltest/runall.sh
@@ -9,19 +9,24 @@ $GOROOT/bin/go install github.com/paypal/hera/worker/mysqlworker
 $GOROOT/bin/go install github.com/paypal/hera/watchdog
 $GOROOT/bin/go install github.com/paypal/hera/mux
 ls -l $GOPATH/bin
-echo $1=one $2=two
-if [ x-$1 == x-all ]
-then
-    suites="bind_eviction_tests strandedchild_tests coordinator_tests saturation_tests adaptive_queue_tests rac_tests sharding_tests"
-    for suite in $suites
-    do
-        ls -F $GOPATH/src/github.com/paypal/hera/tests/functionaltest/$suite | grep /$ | sed -e 's,/,,' | egrep -v '(testutil|no_shard_no_error|set_shard_id_wl|reset_shard_id_wl)' | sed -e "s,^,$suite/," >> toRun
-    done
-else 
-    echo $* > toRun
-fi
+
+cat << EOF > shortRun
+sharding_tests/set_shard_id
+strandedchild_tests/no_free_worker3
+bind_eviction_tests/bind_eviction_disable
+EOF
+
+basedir=$GOPATH/src/github.com/paypal/hera/tests/functionaltest/
+find $basedir -name main_test.go  | sed -e "s,^$basedir,,;s,/main_test.go,," | grep -vFf shortRun > toRun
+grep set_shard_id toRun shortRun
+wc toRun shortRun
+
+# suites="bind_eviction_tests strandedchild_tests coordinator_tests saturation_tests adaptive_queue_tests rac_tests sharding_tests"
+# ls -F $GOPATH/src/github.com/paypal/hera/tests/functionaltest/$suite | grep /$ | sed -e 's,/,,' | egrep -v '(testutil|
+#     no_shard_no_error|set_shard_id_wl|reset_shard_id_wl)' | sed -e "s,^,$suite/," >> toRun
+
 finalResult=0
-for d in `cat toRun`
+for d in `cat shortRun`
 do 
     pushd $GOPATH/src/github.com/paypal/hera/tests/functionaltest/$d
     ln $GOPATH/bin/mysqlworker .
